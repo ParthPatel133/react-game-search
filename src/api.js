a418c21234943d18153210e670a6d78595b86904
parthpatel133 <prpatel133@gmail.com>
2020-12-09 10:39:14 +0530

    setting up API

---
+++ src/api.js
@@ -0,0 +1,34 @@
+//base URL
+const base_url = 'https://api.rawg.io/api/';
+
+//date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+const getCurrentDate = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+//current day-Month-year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDate();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+const pageSize = 10;
+
+//popular games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=${pageSize}`;
+
+export const popularGamesUrl = () => `${base_url}${popular_games}`;
