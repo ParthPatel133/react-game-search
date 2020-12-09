2ed423dcbc82c205581c3f863a0b129778141cda
parthpatel133 <prpatel133@gmail.com>
2020-12-09 11:56:28 +0530

    redux thunk setup, compose, middleware

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 //redux store
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from './reducer';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
