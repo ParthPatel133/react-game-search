59b88e91fbed20cc3136cb34b21de5c479cde426
parthpatel133 <prpatel133@gmail.com>
2020-12-11 22:34:22 +0530

    global styling

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,43 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    *{
+        margin: 0;
+        padding:0;
+        box-sizing: border-box;
+    }
+    html{
+        &::-webkit-scrollbar{
+            width: 0.5rem;
+        }
+        &::-webkit-scrollbar-thumb{
+            background-color: dargrey;
+        }
+    }
+    body{
+        font-family: 'Montserrat', sans-serif;
+        width: 100%;
+    }
+    h2{
+        font-size: 2rem;
+        font-family: 'Abril Fatface', cursive;
+        font-weight: lighter;
+        color: #333;
+    }
+    h3{
+        font-size: 1.2rem;
+        color: #333;
+        padding: 1.1rem;
+    }
+    p{
+        font-size: 1rem;
+        line-height: 180%;
+        color: #696969;
+    }
+    a{
+        text-decoration: none;
+        color: #333;
+    }
+`;
+
+export default GlobalStyles;
